c60334b47c35ab9a3434bc1cba55b0d0c03f896f
Petter Hersinger <petterhersinger@gmail.com>
2022-07-28 15:39:57 +0200

    commit 2

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import UserResults from '../users/UserResults'
+
+function Home() {
+  return (
+    <>
+      {/* SEARCH */}
+      <UserResults />
+    </>
+  )
+}
+
+export default Home
